a91d2d7076fc1d796d7c503afcebb9dbb09f1873
dilia00 <dina_xenia@mail.ru>
2022-09-15 20:54:37 +0300

    Task05 Показывает все целые числа в промежутке от –N до N

---
+++ Task05/Program.cs
@@ -0,0 +1,13 @@
+﻿// 5. Напишите программу, которая на выход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от –N до N.
+// 4 -> «-4, -3, -2, -1, 0, 1, 2, 3, 4»
+// 2-> «-2, -1, 0, 1, 2»
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int current = -number;
+while (current <= number) 
+{
+    Console.Write($"{current}, "); 
+    current++;
+}

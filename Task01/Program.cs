c691992dc2ef3d2c0b696672375b7c59f9c43656
dilia00 <dina_xenia@mail.ru>
2022-09-15 20:00:36 +0300

    Task01 Сравнение числа с квадратом числа

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.Write("Введите первое число ");
+int square = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (square == number*number)
+{
+    Console.WriteLine($"Число {square} является квадратом числа {number}");
+}
+else
+{
+    Console.WriteLine($"Число {square} не является квадратом числа {number}");
+}

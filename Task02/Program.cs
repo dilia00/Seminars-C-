5678a931a43cd1b7e342910043209a6ff96bbb13
dilia00 <dina_xenia@mail.ru>
2022-09-15 21:43:22 +0300

    Task02 Показывает какое число больше

---
+++ Task02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 < num2) Console.WriteLine($"max = {num2} min = {num1}");
+else if(num1 > num2) Console.WriteLine($"max = {num1} min = {num2}");
+else Console.WriteLine("Числа равны, введите разные числа.");

b2cb00cd57bd15ea93e8c1b3d09488cb4293a59a
dilia00 <dina_xenia@mail.ru>
2022-09-15 22:26:33 +0300

    Task02 Показывает какое число больше + редакция

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -10,5 +10,7 @@ int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 if (num1 < num2) Console.WriteLine($"max = {num2} min = {num1}");
-else if(num1 > num2) Console.WriteLine($"max = {num1} min = {num2}");
-else Console.WriteLine("Числа равны, введите разные числа.");
+else 
+if (num1 > num2) Console.WriteLine($"max = {num1} min = {num2}");
+else 
+Console.WriteLine("Числа равны, введите разные числа.");

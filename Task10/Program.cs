6fad6aef8740c78b69f512964e811c9a4ea1dd77
dilia00 <dina_xenia@mail.ru>
2022-09-19 21:38:59 +0300

    Task10 показывает вторую цифру трехзначного числа

---
+++ Task10/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ShowsSecondDigit(int num)
+{
+    if (ThreeDigitNumber(num))
+        return -1;
+    else
+        return (num / 10) % 10;
+}
+
+bool ThreeDigitNumber(int arg)
+{
+    return arg > 999 || arg < 100;
+}
+
+int result = ShowsSecondDigit(number);
+
+if (result == -1)
+    Console.Write("Введите трехзначное число ");
+else
+    Console.WriteLine($"Вторая цифра числа {number} -> {result}");

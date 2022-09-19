93b4d4cd408c219aff87f7746c71be113607a4e2
dilia00 <dina_xenia@mail.ru>
2022-09-19 19:34:15 +0300

    Task12 кратность одного числа второму и остаток от деления

---
+++ Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿// 12. Напишите программу, которая будет принимать на вход два числа и выводить,
+// является ли второе число кратным первому. Если число 2 не кратно числу 1,
+// то программа выводит остаток от деления. 34, 5 -> не кратно, остаток 4 16, 4 -> кратно
+
+
+Console.Write("Введите первое число ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+bool IsAliquot(int arg1, int arg2)
+{
+    return arg1 % arg2 == 0;
+}
+
+if (IsAliquot(number1, number2))
+    Console.Write($"{number1} кратно {number2}");
+else
+    Console.Write($"{number1} не кратно {number2} остаток от деления равен {number1 % number2}");

4207eccea42ff779609588f5e27743361cf7c58b
dilia00 <dina_xenia@mail.ru>
2022-09-19 14:42:03 +0300

    Task09 показывает наибольшую цифру из случайного числа

---
+++ Task09/Program.cs
@@ -0,0 +1,30 @@
+﻿// 9. Напишите программу, которая выводит
+// случайное число из отрезка [10, 99] и показывает
+// наибольшую цифру числа.
+// 78 -> 8
+// 12-> 2
+// 85 -> 8
+
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (IsEqualDigits(firstDigit, secondDigit))
+        return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+bool IsEqualDigits(int num1, int num2)
+{
+    return num1 == num2;
+}
+
+int number = new Random().Next(10, 100);
+int maxDigit = MaxDigit(number);
+
+//string result = maxDigit > 0 ? maxDigit.ToString() : "Цифры равны";
+if (maxDigit > 0)
+    Console.WriteLine($"Наибольшая цифра числа {number} = {maxDigit}");
+else
+    Console.WriteLine($"В числе {number} цифры равны");

be4d1b76378f5eb08831bd497c9337a1f4cc57ae
dilia00 <dina_xenia@mail.ru>
2022-09-20 19:32:12 +0300

    показывает третью цифру любого числа + исправила недочет

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,12 +3,13 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
 Console.Write("Введите любое число ");
 long number = Convert.ToInt64(Console.ReadLine());
 
 long GetThreeDigitNumber(long arg)
 {
-    while (arg >= 999)
+    while (arg > 999 || arg < -999)
     {
         arg = arg / 10;
     }
@@ -21,9 +22,11 @@ int ThirdDigit(int num)
     return thirdDigit;
 }
 
-long result = GetThreeDigitNumber(number);
-int res = ThirdDigit(Convert.ToInt32(result));
-if (number < 100)
-    Console.Write($"У числа {number} третьей цифры нет");
-else
+if (number > 99 || number < -99)
+{
+    long result = GetThreeDigitNumber(number);
+    int res = ThirdDigit(Convert.ToInt32(result));
     Console.WriteLine($"Третья цифра числа {number} -> {res}");
+}
+else
+    Console.Write($"У числа {number} третьей цифры нет");

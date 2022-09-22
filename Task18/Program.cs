9faa15461ae9c1509b488870ac5cdd57337db660
dilia00 <dina_xenia@mail.ru>
2022-09-22 13:40:31 +0300

    Task18 по заданному номеру четверти, показывает диапазон координат

---
+++ Task18/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти в диапозоне от 1 до 4 включительно ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string Quater(int num)
+{
+    if (num == 1)
+        return "Диапазон равен х > 0, у > 0 ";
+    if (num == 2)
+        return "Диапазон равен х < 0, у > 0 ";
+    if (num == 3)
+        return "Диапазон равен х < 0, у > 0 ";
+    if (num == 4)
+        return "Диапазон равен х > 0, у < 0 ";
+    return "Введите верный номр четверти";
+}
+
+string result = Quater(number);
+Console.WriteLine(result);

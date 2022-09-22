a3ccc5f66d9f3fd9b0ad636d7e4811d59f780604
dilia00 <dina_xenia@mail.ru>
2022-09-22 13:59:22 +0300

    Task22  принимает число (N) и выдаёт таблицу квадратов чисел от 1 до N

---
+++ Task22/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void SquareTable(int arg)
+{
+    int counter = 1;
+    while (counter <= arg)
+    {
+        Console.WriteLine($"|{counter, 3} | {counter * counter, 5}|");
+        counter++;
+    }
+}
+if (number > 0)
+    SquareTable(number);
+else
+    Console.Write("Введите корректное число ");

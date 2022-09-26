cf991d2b9f9b3deaacd90f6717170c71194302e3
dilia00 <dina_xenia@mail.ru>
2022-09-26 15:21:51 +0300

    Task28 принимает на вход число и выдаёт сумму цифр в числе

---
+++ Task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите двухзначное и более число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 9)
+{
+    int res = GetSumDigits(number);
+    Console.WriteLine($"Сумма цифр числа {number} = {res}");
+}
+else
+    Console.WriteLine("Введено некорректное значение");
+
+int GetSumDigits(int num)
+{
+    int sum = default;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}

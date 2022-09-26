1aa6137c33077b9584bb40a4883d77e4b9595374
dilia00 <dina_xenia@mail.ru>
2022-09-26 14:40:57 +0300

    Task29 выводит массив из 8 элементов, заполненный случайном порядке

---
+++ Task29/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
+
+void GetArray(int[] arr)
+{
+    int length = arr.Length;
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    Console.Write("[ ");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}

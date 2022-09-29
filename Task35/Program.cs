dc564753dfb79d9207b61ff4d3ad90ce2aa5fd50
dilia00 <dina_xenia@mail.ru>
2022-09-29 17:30:38 +0300

    Task35 Найти количество элементов массива, значения которых лежат в заданном отрезке

---
+++ Task35/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых лежат в
+// отрезке [10,99].
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для
+// 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    var rnd = new Random(); // var или Random тип данных с левой стороны
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int RangeBotTop(int[] arr, int min, int max)
+{
+    int count = default;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] >= min && arr[i] <= max)
+            count++;
+    }
+    return count;
+}
+
+int[] arr = CreateArrayRndInt(123, 1, 123);
+PrintArray(arr);
+int result = RangeBotTop(arr, 10, 99);
+System.Console.WriteLine(result);

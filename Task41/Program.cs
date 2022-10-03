3b613181e30bace9f8651ae012e30433df5423ae
dilia00 <dina_xenia@mail.ru>
2022-10-03 18:02:41 +0300

    Task41 сколько положительных чисел из введенных пользователем

---
+++ Task41/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте,
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Введите какое количестао чисел вы хотите ввести: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+for (int i = 0; i < size; i++)
+{
+    Console.Write($"Введите {i + 1}-e число из {size}: ");
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int GetNumberOfPositiveNumbers(int[] array)
+{
+    int count = default;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (array[i] > 0)
+            count++;
+    }
+    return count;
+}
+Console.WriteLine();
+PrintArray(arr);
+Console.WriteLine($"Количество положительных чисел = {GetNumberOfPositiveNumbers(arr)}");

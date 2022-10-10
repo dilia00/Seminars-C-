9df6ac09f60c6765d1a22b4583c0d5e1abd38ebe
dilia00 <dina_xenia@mail.ru>
2022-10-10 14:38:13 +0300

    покажет сколько раз встречается элементы в двумерном массиве

---
+++ Task57/Program.cs
@@ -0,0 +1,92 @@
+﻿// Задача 57: Составить частотный словарь элементов
+// двумерного массива. Частотный словарь содержит
+// информацию о том, сколько раз встречается элемент
+// входных данных.
+
+int[,] myArray = CreateMatrix(4, 4, 1, 9);
+PrintMatrix(myArray);
+Console.WriteLine();
+int[] myArr = MatrixToOneDemensionArray(myArray);
+PrintArray(myArr);
+Array.Sort(myArr);
+Console.WriteLine();
+PrintArray(myArr);
+Console.WriteLine();
+ElementsDictionary(myArr);
+
+void ElementsDictionary(int[] array)
+{
+    int count = 1;
+    int current = array[0];
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] == current) count++;
+        else
+        {
+            Console.WriteLine($"Количество {current} = {count}");
+            current = array[i];
+            count = 1;
+        }
+    }
+    Console.WriteLine($"Количество {current} = {count}");
+}
+
+int[] MatrixToOneDemensionArray(int[,] matrix)
+{
+    int[] array = new int[matrix.Length];
+    int k = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            array[k] = matrix[i, j];
+            k++;
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  //rows (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //columns (1)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("|");
+    }
+}
+

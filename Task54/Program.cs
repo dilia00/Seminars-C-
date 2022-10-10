5112d3462dc875307add0139388e05eae5dcaa48
dilia00 <dina_xenia@mail.ru>
2022-10-10 19:02:33 +0300

    сортирует по убыванию элементы каждой строки двумерного массива

---
+++ Task54/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] myMatrix = CreateMatrix(5, 4, 1, 9);
+PrintMatrix(myMatrix);
+Console.WriteLine();
+SelectionSortMaxMatrix(myMatrix);
+PrintMatrix(myMatrix);
+
+void SelectionSortMaxMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)  //rows (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            int maxPosition = j;
+            for (int k = j + 1; k < matrix.GetLength(1); k++)
+            {
+                if (matrix[i, k] > matrix[i, maxPosition])
+                    maxPosition = k;
+            }
+            int temp = matrix[i, j];
+            matrix[i, j] = matrix[i, maxPosition];
+            matrix[i, maxPosition] = temp;
+        }
+    }
+}
+
+
+int[,] CreateMatrix(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    var rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)  //rows (0)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) //columns (1)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+
